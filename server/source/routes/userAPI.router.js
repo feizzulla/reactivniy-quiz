fda68065a5b52d97581c6b707d7b4c0376fa9f1f
feizzulla <feizzulla@yandex.ru>
2024-07-25 20:40:56 +0300

    server-userAPIrouter users GET POST

--- server/source/routes/userAPI.router.js
+++ server/source/routes/userAPI.router.js
@@ -1,12 +1,27 @@
 const router = require("express").Router();
-const { user } = require("../../db/models");
+const { User } = require("../../db/models");
 
-router.route("/user").get(async (req, res) => {
-  try {
-  } catch (error) {
-    console.log(`${error}`);
-    res.status(200).json(error);
-  }
-});
+router
+  .route("/users")
+  .get(async (req, res) => {
+    try {
+      const users = await User.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+      });
+      res.status(200).json(users);
+    } catch (error) {
+      console.log(`${error}`);
+      res.status(400).json(error);
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const newUser = req.body;
+      const addUser = await User.create(newUser);
+      res.status(201).json(addUser);
+    } catch (error) {
+      res.status(400).json({ result: false, error: "Ошибка запроса." });
+    }
+  });
 
 module.exports = router;
