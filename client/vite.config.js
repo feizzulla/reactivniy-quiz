32f2bee016254a9f1a49aa7edac211d0667e8094
GertiyMV <gertiymv@mail.ru>
2024-07-26 11:44:58 +0400

    theme server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        [process.env.VITE_API]: {
+          target: process.env.VITE_TARGET,
+          changeOrigin: true,
+        },
+      },
+      port: parseInt(process.env.VITE_PORT),
+    },
+  });
+};
