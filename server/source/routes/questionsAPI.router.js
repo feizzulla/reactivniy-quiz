f0807dfc8b9891440592a07bf2d9c67ac2f9bb89
feizzulla <feizzulla@yandex.ru>
2024-07-26 11:01:12 +0300

    questions router

--- server/source/routes/questionsAPI.router.js
+++ server/source/routes/questionsAPI.router.js
@@ -1,6 +1,28 @@
 const router = require("express").Router();
-const { Statistic, User } = require("../../db/models");
+const { Qa: Questions } = require("../../db/models");
 
-router.route("/");
+router.route("/").get(async (req, res) => {
+  try {
+    const questions = await Questions.findAll({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    res.status(200).json(questions);
+  } catch (error) {
+    console.log(`${error}`);
+    res.status(400).json(error);
+  }
+});
+
+router.route("/:id").get(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const question = await Questions.findByPk(id, {
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    res.status(200).json(question);
+  } catch (error) {
+    res.status(400).json({ result: false, error: error });
+  }
+});
 
 module.exports = router;
