7b92ab6e6ae2f698ea0c5a704738b44be31590ab
GertiyMV <gertiymv@mail.ru>
2024-07-26 12:01:26 +0400

    return dev

--- client/src/pages/EnterPage/EnterPage.jsx
+++ client/src/pages/EnterPage/EnterPage.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function EnterPage() {
-  return <div>Auth Init</div>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await axios.get(`${import.meta.env.VITE_API}/users`);
+    return response.data;
+  };
+
+  useEffect(() => {
+    getUsers().then((data) => setUsers(data));
+  }, []);
+
+  console.log(users);
+
+
+  return <div>
+    <form>
+      Введи почту:
+      <input name="newAnswer" autoComplete="off" />
+    </form>
+  </div>;
 }
+
+{/* <div>
+<div>
+  <img style={{ weidth: "60px", height: "60px" }} src={el.imageURL} alt={el.theme} />
+</div>
+<form onSubmit={submitHandler}>
+  <div>{el.question}</div>
+  <input name="newAnswer" autoComplete="off"></input>
+  <Button type={"submit"} color={response} text={"ответить"} onClick={newQuestion} />
+</form>
+</div> */}
