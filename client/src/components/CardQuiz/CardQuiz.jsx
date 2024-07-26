ce2df84e5b5c9878389e53f9f1d6b9731db00b52
feizzulla <feizzulla@yandex.ru>
2024-07-26 13:25:17 +0300

    server-checkanswers GPT

--- client/src/components/CardQuiz/CardQuiz.jsx
+++ client/src/components/CardQuiz/CardQuiz.jsx
@@ -15,43 +15,45 @@ export default function CardTheme({ el, newQuestion }) {
 
   const [response, setResponse] = useState();
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    if (event.target.childNodes[1].value === answer) {
-      setResponse("green");
-    } else setResponse("red");
-  };
-//   const submitHandler = async (event) => {
-//     event.preventDefault();
-//     const isCorrect = await axios.post(`${import.meta.env.VITE_API}/checkanswer`, 
-// {
-// "question": "Любой вопрос",
-// "userAnswer": "Любой ответ пользователя, в числах или строкой.",
-// "correctAnswer": "Правильный ответ. Формировать ответ лучше текстом на русском языке."
-// }
-
-// {
-// "question": `${el.question}`,
-// "userAnswer": `${event.target.childNodes[1].value}`,
-// "correctAnswer": `${el.answer}`
-// }
-
-  //   );
-  //   console.log(isCorrect);
+  // const submitHandler = (event) => {
+  //   event.preventDefault();
   //   if (event.target.childNodes[1].value === answer) {
   //     setResponse("green");
   //   } else setResponse("red");
   // };
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const isCorrect = await axios.post(
+      `${import.meta.env.VITE_API}/checkanswer`,
+      {
+        question: `${el.question}`,
+        userAnswer: `${event.target.childNodes[1].value}`,
+        correctAnswer: `${el.answer}`,
+      }
+    );
+    if (isCorrect.data.correct) {
+      setResponse("green");
+    } else setResponse("red");
+  };
 
   return (
     <div>
       <div>
-        <img style={{ weidth: "60px", height: "60px" }} src={el.imageurl} alt={el.theme} />
+        <img
+          style={{ weidth: "60px", height: "60px" }}
+          src={el.imageurl}
+          alt={el.theme}
+        />
       </div>
       <form onSubmit={submitHandler}>
         <div>{el.question}</div>
         <input name="newAnswer" autoComplete="off"></input>
-        <Button type={"submit"} color={response} text={"ответить"} onClick={newQuestion} />
+        <Button
+          type={"submit"}
+          color={response}
+          text={"ответить"}
+          onClick={newQuestion}
+        />
       </form>
     </div>
   );
